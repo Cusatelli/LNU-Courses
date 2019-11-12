cedfd7e19fcda1478aba315f744261c09c913173
Cusatelli <einarvandevelde@gmail.com>
2019-11-12 02:41:47 +0100

    Renamed RegistryHandler.java to FileHandler.java

--- 1DV607/src/workshop2/controller/FileHandler.java
+++ 1DV607/src/workshop2/controller/FileHandler.java
@@ -1,15 +1,48 @@
 package workshop2.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class FileHandler {
-	File file = new File("");
 	
-	public void readFromDatabase() {
+	private String path;
+	private File file;
 	
+	public FileHandler(String path) {
+		this.path = path;
+		file = new File(this.path + ".dat");
 	}
 	
-	public void writeToDatabase() {
+	public void writeToFile(Object object) {
+		ObjectOutputStream objectOutputStream;
+		try {
+			objectOutputStream = new ObjectOutputStream(new FileOutputStream(file));
+			objectOutputStream.writeObject(object);
+			objectOutputStream.close();
 		
+		} catch (IOException e) { 
+			e.printStackTrace(); 
+		}
+	}
+	
+	public Object readFromFile() {
+		ObjectInputStream objectInputStream;
+		Object object = null;
+		try {
+			objectInputStream = new ObjectInputStream(new FileInputStream(file));
+			object = objectInputStream.readObject();
+		
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return object;
+	}
+	
+	public boolean containsFile() {
+		return file.isFile() && file.canRead();
 	}
 }
